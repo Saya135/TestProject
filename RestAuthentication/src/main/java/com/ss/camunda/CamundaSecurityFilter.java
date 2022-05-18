3380c2119826cf0925b86d1e4213073068e4f5a1
Saya135 <94954460+Saya135@users.noreply.github.com>
2022-05-18 09:45:18 +0900

    Create CamundaSecurityFilter.java

---
+++ RestAuthentication/src/main/java/com/ss/camunda/CamundaSecurityFilter.java
@@ -0,0 +1,33 @@
+package com.ss.camunda;
+
+
+import javax.servlet.Filter;
+
+import org.camunda.bpm.engine.rest.security.auth.ProcessEngineAuthenticationFilter;
+import org.camunda.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CamundaSecurityFilter
+{
+
+	@Bean
+	public FilterRegistrationBean<ProcessEngineAuthenticationFilter> processEngineAuthenticationFilter()
+	{
+		FilterRegistrationBean<ProcessEngineAuthenticationFilter> registration = new FilterRegistrationBean<>();
+		registration.setName("camunda-auth");
+		registration.setFilter(this.getProcessEngineAuthenticationFilter());
+		registration
+			.addInitParameter("authentication-provider", HttpBasicAuthenticationProvider.class.getName());
+		registration.addUrlPatterns("/engine-rest/*");
+		return registration;
+	}
+
+	@Bean
+	public ProcessEngineAuthenticationFilter getProcessEngineAuthenticationFilter()
+	{
+		return new ProcessEngineAuthenticationFilter();
+	}
+}
